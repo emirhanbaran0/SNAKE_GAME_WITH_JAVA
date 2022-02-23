b80dc86b313ed6d7d9d941ae603531bc4482f051
Emirhan Baran <92358029+emirhanbaran0@users.noreply.github.com>
2022-02-23 20:32:44 +0300

    Create main.java
    In the main section of my project, I just generate an object for playing our game.

---
+++ main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Main   {
+
+	public static void main(String[] args) {
+		MainScreen frame=null;
+		frame.singleScreen();
+		
+	}
+
+}
